46b6955391f7ec527c7cdc65bc4a2c501114357a
ShivramS2006 <shivrams.2006@gmail.com>
2022-01-11 13:36:34 +0530

    Format all `.py` files
     - Properly format multi-line declarations
     - Add newlines wherever necessary
     - Make usage of spaces consistent
     - Remove unnecessary parens in `if` statements
     - Split long statements into multiple lines

--- cogs/eval.py
+++ cogs/eval.py
@@ -2,40 +2,42 @@ from time import time
 from discord.ext import commands
 import discord
 
+
 class EvalCommand(commands.Cog):
-    def __init__(self,client):
-        self.client=client
+    def __init__(self, client):
+        self.client = client
 
     def resolve_variable(self, variable):
         if hasattr(variable, "__iter__"):
             var_length = len(list(variable))
             if (var_length > 100) and (not isinstance(variable, str)):
                 return f"<a {type(variable).__name__} iterable with more than 100 values ({var_length})>"
-            elif (not var_length):
+            elif not var_length:
                 return f"<an empty {type(variable).__name__} iterable>"
 
         if (not variable) and (not isinstance(variable, bool)):
             return f"<an empty {type(variable).__name__} object>"
-        return (variable if (len(f"{variable}") <= 1000) else f"<a long {type(variable).__name__} object with the length of {len(f'{variable}'):,}>")
+        return (
+            variable
+            if (len(f"{variable}") <= 1000)
+            else f"<a long {type(variable).__name__} object with the length of {len(f'{variable}'):,}>"
+        )
 
     def prepare(self, string):
-        arr = string.strip("```").replace("py\n", "").replace("python\n", "").split("\n")
+        arr = (
+            string.strip("```").replace("py\n", "").replace("python\n", "").split("\n")
+        )
         if not arr[::-1][0].replace(" ", "").startswith("return"):
             arr[len(arr) - 1] = "return " + arr[::-1][0]
         return "".join(f"\n\t{i}" for i in arr)
 
-    @commands.command(pass_context=True, aliases=['eval',  'evaluate'])
+    @commands.command(pass_context=True, aliases=["eval", "evaluate"])
     @commands.is_owner()
     async def _eval(self, ctx, *, code: str):
-        silent = ("-s" in code)
+        silent = "-s" in code
 
         code = self.prepare(code.replace("-s", ""))
-        args = {
-            "discord": discord,
-            "client" : self.client,
-
-            "ctx": ctx
-        }
+        args = {"discord": discord, "client": self.client, "ctx": ctx}
 
         try:
             exec(f"async def func():{code}", args)
@@ -44,15 +46,28 @@ class EvalCommand(commands.Cog):
             if silent or (response is None) or isinstance(response, discord.Message):
                 del args, code
                 return
-            input = code.replace("    return","")
-            embed= discord.Embed(color= 0x2f3136 , title="Evaluating Data",description=f"Response time : {round(self.client.latency*1000)} ms")
-            embed.add_field(name="Input",value=f"```py\n{input}```",inline=False)
-            embed.add_field(name="Output :",value=f"```py\n{self.resolve_variable(response)}```").set_footer(text=f"DATA TYPE : {type(response).__name__}")
+            input = code.replace("    return", "")
+            embed = discord.Embed(
+                color=0x2F3136,
+                title="Evaluating Data",
+                description=f"Response time : {round(self.client.latency*1000)} ms",
+            )
+            embed.add_field(name="Input", value=f"```py\n{input}```", inline=False)
+            embed.add_field(
+                name="Output :", value=f"```py\n{self.resolve_variable(response)}```"
+            ).set_footer(text=f"DATA TYPE : {type(response).__name__}")
             await ctx.send(embed=embed)
         except Exception as e:
-            await ctx.send(embed=discord.Embed(color=0x2f3136,title=f"Error occurred:",description=f"```\n{type(e).__name__}: {str(e)}```"))
+            await ctx.send(
+                embed=discord.Embed(
+                    color=0x2F3136,
+                    title=f"Error occurred:",
+                    description=f"```\n{type(e).__name__}: {str(e)}```",
+                )
+            )
 
         del args, code, silent
+
 
 def setup(client):
     client.add_cog(EvalCommand(client))
--- cogs/moderator_commands.py
+++ cogs/moderator_commands.py
@@ -2,33 +2,37 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import has_permissions, CheckFailure
 
-class Moderator_commands(commands.Cog):
 
-    def __init__(self , client):
+class Moderator_commands(commands.Cog):
+    def __init__(self, client):
         self.client = client
 
-
     @commands.command()
-    async def clear(self, ctx, ammount = 2):
+    async def clear(self, ctx, ammount=2):
         channel = ctx.channel
-        await channel.purge(limit = ammount)
+        await channel.purge(limit=ammount)
 
     @clear.error
-    async def clear_error(self,ctx,error):
+    async def clear_error(self, ctx, error):
         if isinstance(error, commands.BadArgument):
-            embed = discord.Embed(title="⚠️",
-            description = '"The no. of messages you want to delete should be a numerical no."', color = discord.Color.gold())
-            await ctx.send(embed = embed)
+            embed = discord.Embed(
+                title="⚠️",
+                description='"The no. of messages you want to delete should be a numerical no."',
+                color=discord.Color.gold(),
+            )
+            await ctx.send(embed=embed)
 
     @commands.command()
-    @has_permissions(manage_roles = True)
-    async def mute(self, ctx, member:discord.Member, *,reason = None):
+    @has_permissions(manage_roles=True)
+    async def mute(self, ctx, member: discord.Member, *, reason=None):
 
-        role = discord.utils.get(member.guild.roles, name = 'Muted')
+        role = discord.utils.get(member.guild.roles, name="Muted")
         if not role:
-            muted_role= await ctx.guild.create_role(name= "Muted", color= discord.Color.greyple())
+            muted_role = await ctx.guild.create_role(
+                name="Muted", color=discord.Color.greyple()
+            )
             for channel in ctx.guild.channels:
-                await channel.set_permissionmuted(Muted,send_messages=False)
+                await channel.set_permissionmuted(Muted, send_messages=False)
 
         else:
             pass
@@ -36,94 +40,109 @@ class Moderator_commands(commands.Cog):
         await member.add_roles(role)
         react = "👍"
         await ctx.message.add_reaction(react)
-                                                                                       # await member.edit(mute = True)
+        # await member.edit(mute = True)
         channel = await member.create_dm()
-        embed = discord.Embed(title= "You Have Been Muted!!",
-        description= f"You have been muted by {ctx.author.mention} from {member.guild.name} \nReason: {reason}",
-        colour= discord.Colour.dark_grey())
-        embed.set_author(name =f"{ctx.author.name}",icon_url= ctx.author.avatar_url)
-        await channel.send(embed= embed)
+        embed = discord.Embed(
+            title="You Have Been Muted!!",
+            description=f"You have been muted by {ctx.author.mention} from {member.guild.name} \nReason: {reason}",
+            colour=discord.Colour.dark_grey(),
+        )
+        embed.set_author(name=f"{ctx.author.name}", icon_url=ctx.author.avatar_url)
+        await channel.send(embed=embed)
 
     @commands.command()
-    @has_permissions(manage_roles= True)
-    async def unmute(self, ctx, member:discord.Member):
+    @has_permissions(manage_roles=True)
+    async def unmute(self, ctx, member: discord.Member):
         member.remove_roles(discord.utils.get(ctx.guild.roles, name="Muted"))
         ctx.message.add_reaction("👍")
         channael = await member.create_dm()
-        embed = discord.Embed(title= "congratulations!!",
-        discription=f"You have been unmuted from {member.guild.name}!!",
-        color= discord.Color.red())
-        channel.send(embed = embed)
-
+        embed = discord.Embed(
+            title="congratulations!!",
+            discription=f"You have been unmuted from {member.guild.name}!!",
+            color=discord.Color.red(),
+        )
+        channel.send(embed=embed)
 
     @commands.command()
     @has_permissions(ban_members=True)
-    async def ban(self,ctx , member : discord.Member, *,reason = None):
+    async def ban(self, ctx, member: discord.Member, *, reason=None):
         members = ctx.guild.members
-        if ctx.author == member :
-            embed= discord.Embed(title= "⚠️",
-            discription=f"you cant ban yourself!!",
-            color= discord.Color.gold())
+        if ctx.author == member:
+            embed = discord.Embed(
+                title="⚠️",
+                discription=f"you cant ban yourself!!",
+                color=discord.Color.gold(),
+            )
 
         elif member not in members:
             await ctx.send(f"Member is not even in the server")
 
-        else :
-            await member.ban(reason = reason)
-            channel= await member.create_dm()
+        else:
+            await member.ban(reason=reason)
+            channel = await member.create_dm()
             await ctx.message.add_reaction("👍")
-            embed = discord.Embed(title= "**You Have Been Banned!!**",
-            description= f"You have been banned by {ctx.author.mention} from {member.guild.name} \nReason: {reason}",
-            colour= discord.Colour.dark_grey())
-            embed.set_author(name =f"{ctx.author.name}",icon_url= f"{ctx.author.avatar_url}")
-            await channel.send(embed= embed)
+            embed = discord.Embed(
+                title="**You Have Been Banned!!**",
+                description=f"You have been banned by {ctx.author.mention} from {member.guild.name} \nReason: {reason}",
+                colour=discord.Colour.dark_grey(),
+            )
+            embed.set_author(
+                name=f"{ctx.author.name}", icon_url=f"{ctx.author.avatar_url}"
+            )
+            await channel.send(embed=embed)
+
     @ban.error
-    async def ban_error(self,ctx,error):
+    async def ban_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
-            embed = discord.Embed(title="⚠️",
-            description = f"{ctx.author.mention}, you have to mention someone to ban them!! \nExample: ```.ban @user reason``` (you can choose whether you want to mention the reason) ",
-            color = discord.Color.gold())
-            await ctx.send(embed = embed)
+            embed = discord.Embed(
+                title="⚠️",
+                description=f"{ctx.author.mention}, you have to mention someone to ban them!! \nExample: ```.ban @user reason``` (you can choose whether you want to mention the reason) ",
+                color=discord.Color.gold(),
+            )
+            await ctx.send(embed=embed)
 
     @commands.command()
-    @has_permissions(ban_members= True)
+    @has_permissions(ban_members=True)
     async def unban(self, ctx, *, member):
         found = "not"
         banned_users = await ctx.guild.bans()
-        member_name , member_discriminator = member.split('#')
+        member_name, member_discriminator = member.split("#")
 
         for banned_user in banned_users:
             user = banned_user.user
-            if (user.name , user.discriminator) == (member_name , member_discriminator):
+            if (user.name, user.discriminator) == (member_name, member_discriminator):
                 await ctx.guild.unban(user)
                 await ctx.message.add_reaction("👍")
                 found = "found"
 
-        if found =="not":
-            embed = discord.Embed(title="**Member not fount!!**⚠️",
-            discription= "The member was not found, please make sure that the member is still banned. \nSyntax: ```.ban UserName#disciminator```",
-            color= discord.Color.gold() )
-
+        if found == "not":
+            embed = discord.Embed(
+                title="**Member not fount!!**⚠️",
+                discription="The member was not found, please make sure that the member is still banned. \nSyntax: ```.ban UserName#disciminator```",
+                color=discord.Color.gold(),
+            )
 
     @commands.command()
-    @has_permissions(kick_members= True)
-    async def kick(self, ctx , member : discord.Member, *,reason = None):
+    @has_permissions(kick_members=True)
+    async def kick(self, ctx, member: discord.Member, *, reason=None):
         members = ctx.guild.members
 
-        if ctx.author == member :
+        if ctx.author == member:
             ctx.message.add_reaction("⚠️")
 
         elif member not in members:
             await ctx.send(f"Member is not even in the server")
 
-        else :
-            await member.kick(reason = reason)
+        else:
+            await member.kick(reason=reason)
             channel = await member.create_dm()
-            embed = discord.Embed(title= "**You've been kicked!!**",
-            discription= f"You were kicked out of {ctx.message.guild} \nby: {ctx.author.name} \nReason: {reason}",
-            colour= discord.Color.darker_grey())
-            channel.send(embed = embed)
+            embed = discord.Embed(
+                title="**You've been kicked!!**",
+                discription=f"You were kicked out of {ctx.message.guild} \nby: {ctx.author.name} \nReason: {reason}",
+                colour=discord.Color.darker_grey(),
+            )
+            channel.send(embed=embed)
 
 
 def setup(client):
-     client.add_cog(Moderator_commands(client))
+    client.add_cog(Moderator_commands(client))
